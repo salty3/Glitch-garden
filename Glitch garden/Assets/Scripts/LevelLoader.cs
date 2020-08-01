5b6eacf097841a2063d232604d6cfbea395f803f
salty3 <mistick115@gmail.com>
2020-08-02 01:01:45 +0300

    Menu

---
+++ Glitch garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float secondsToLoad = 3f;
+
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(Loading());
+        }
+    }
+
+    IEnumerator Loading()
+    {
+        yield return new WaitForSeconds(secondsToLoad);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
