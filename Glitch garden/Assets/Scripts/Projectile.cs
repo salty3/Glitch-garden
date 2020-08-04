44fb40ac515c036791775a98d6e65fb3cc614468
salty3 <mistick115@gmail.com>
2020-08-04 23:51:25 +0300

    Projectile and cactus animation

---
+++ Glitch garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed, rotationSpeed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
+        transform.Rotate(new Vector3(0, 0, rotationSpeed));
+    }
+}
---
+++ Glitch garden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Projectile projectile;
+    [SerializeField] private GameObject gun;
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
