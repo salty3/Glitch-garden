4a852b58772e7b0928414db541669f59413c3ab9
salty3 <mistick115@gmail.com>
2020-08-05 23:52:01 +0300

    Health & damage

---
+++ Glitch garden/Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+}
--- Glitch garden/Assets/Scripts/Projectile.cs
+++ Glitch garden/Assets/Scripts/Projectile.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed, rotationSpeed;
+    [SerializeField] private int damage = 100;
 
     private void Update()
+    {
+        Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        var attacker = collision.GetComponent<Attacker>();
+        if (health && attacker)
+        {
+            health.DealDamage(damage);
+        }
+    }
+
+    private void Move()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
         transform.Rotate(new Vector3(0, 0, rotationSpeed));
     }
+
+    
 }
